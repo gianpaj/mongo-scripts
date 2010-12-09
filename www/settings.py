51b1ba543559d7618702939711a32b26585bcdb1
Buildbot <mike@dirolf.com>
2010-12-08 22:07:46 -0500

    cleaning

--- www/settings.py
+++ www/settings.py
@@ -7,6 +7,6 @@ devel = os.path.exists(os.path.join(here, 'devel'))
 if devel:
     usagedb_host = wwwdb_host = mongowwwdb_host = "localhost"
 else:
-    usagedb_host = "jira-e.10gen.cc"
-    wwwdb_host = "www-e.10gen.cc"
+    usagedb_host = "jira.10gen.cc"
+    wwwdb_host = "www-c.10gen.cc"
     mongowwwdb_host = "www-m1.10gen.cc"
