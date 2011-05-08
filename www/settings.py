fa5734aa2c40719222e906663c2aa4ded9584bad
www <www@www-c1.10gen.cc>
2011-05-08 03:08:09 +0000

    newer

--- www/settings.py
+++ www/settings.py
@@ -13,5 +13,5 @@ if devel:
     usagedb_host = wwwdb_host = mongowwwdb_host = "localhost"
 else:
     usagedb_host = "jira.10gen.cc"
-    wwwdb_host = "www-c.10gen.cc"
-    mongowwwdb_host = "www-m1.10gen.cc"
+    wwwdb_host = "localhost"
+    mongowwwdb_host = "localhost"
