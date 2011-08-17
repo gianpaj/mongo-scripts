b1a22e47a739390def60d6f5d0055ab5ddc00346
Dan Pasette <dan@10gen.com>
2011-08-17 16:57:03 -0400

    added mms link and changed link color

--- chrome-ext/jira-ext/contentscript.js
+++ chrome-ext/jira-ext/contentscript.js
@@ -3,10 +3,24 @@ var doWork = function() {
     var companyElem = document.getElementById("customfield_10030-field");
     if (companyElem != null) {
         var companyName = companyElem.firstElementChild.innerHTML;
-        var cNode = companyElem.firstElementChild.cloneNode(true);
-        cNode.href = "http://www.10gen.com/clienthub/link/jira/" + companyName;
-        cNode.innerHTML = "(clienthub)";
-        companyElem.appendChild(cNode);
+
+        // create clienthub link
+        var clienthubLink = companyElem.firstElementChild.cloneNode(true);
+        clienthubLink.href = "http://www.10gen.com/clienthub/link/jira/" + companyName;
+        clienthubLink.innerHTML = "clienthub";
+        clienthubLink.style.color = "red";
+
+        // create mms link
+        var mmsLink = clienthubLink.cloneNode(true);
+        mmsLink.href = "https://mms.10gen.com/links/customer?id=" + companyName + ",425989";
+        mmsLink.innerHTML = "mms";
+
+        // add links
+        companyElem.appendChild(document.createTextNode("("));
+        companyElem.appendChild(clienthubLink);
+        companyElem.appendChild(document.createTextNode(" | "));
+        companyElem.appendChild(mmsLink);
+        companyElem.appendChild(document.createTextNode(")"));
     }
 }
 
