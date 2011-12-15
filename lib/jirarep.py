ae59ffd930b977b0cf31dfe17a13d04909beaaae
Mike O'Brien <mikeo@10gen.com>
2011-12-15 17:20:35 -0500

    add option to run recent jira issues only, fix formatting

--- lib/jirarep.py
+++ lib/jirarep.py
@@ -12,76 +12,78 @@ import sys
 
 
 class JiraReport(CorpBase):
+
     def GET(self):
         web.header('Content-type','text/html')
         engineerslist = eng_group
         companieslist = list(jirareportsdb.companies.find());
         if not companieslist:
-          companieslist = list(jirareportsdb.issues.distinct("company"))
-          refresh_companies()
-          companieslist = jirareportsdb.companies.find();
+            companieslist = list(jirareportsdb.issues.distinct("company"))
+            refresh_companies()
+            companieslist = jirareportsdb.companies.find();
 
         periodslist = [p['_id'] for p in jirareportsdb.periods.find()]
         if not periodslist:
-          refresh_periods()
-          periodslist = [p['_id'] for p in jirareportsdb.periods.find()]
+            refresh_periods()
+            periodslist = [p['_id'] for p in jirareportsdb.periods.find()]
         periodslist.sort(reverse=True)
 
         t = env.get_template("jirareports.html" )
         return t.render(dict(message="hello", engineers=engineerslist, companies=companieslist, periods=periodslist))
 
 def refresh_companies():
-  companieslist = list(jirareportsdb.issues.distinct("company"))
-  for c in companieslist:
-      try:
-          jirareportsdb.companies.insert({"_id":c})
-      except Exception, e:
-          pass
+    print "rebuilding copmany list"
+    companieslist = list(jirareportsdb.issues.distinct("company"))
+    for c in companieslist:
+        try:
+            jirareportsdb.companies.insert({"_id":c})
+        except Exception, e:
+            pass
 
 def refresh_periods():
-  issues = jirareportsdb.issues.find({},{"created":1})
-  periods = set({})
-  for issue in issues:
-    periodname = "%s-%s" % ( issue["created"].year , issue["created"].month )
-    periods.add(periodname)
-  for period in periods:
-    jirareportsdb.periods.insert({"_id":period})
+    issues = jirareportsdb.issues.find({},{"created":1})
+    periods = set({})
+    for issue in issues:
+        periodname = "%s-%s" % ( issue["created"].year , issue["created"].month )
+        periods.add(periodname)
+    for period in periods:
+        jirareportsdb.periods.insert({"_id":period})
 
 
 class JiraEngineerReport(CorpBase):
-  def GET(self, name):
-    web.header('Content-type','application/json')
-    pageparams = web.input()
-    timeperiod =  pageparams.get("period", None) or "all"
-    info = jirareportsdb.reports.find_one({'period':'all', 'engineer':name})
-    time_filter = {}
+    def GET(self, name):
+        web.header('Content-type','application/json')
+        pageparams = web.input()
+        timeperiod =  pageparams.get("period", None) or "all"
+        info = jirareportsdb.reports.find_one({'period':'all', 'engineer':name})
+        time_filter = {}
 
-    report = jirareportsdb.reports.find_one({"type":"engineer", "name":name, "period":timeperiod})
-    if (report is None or
-        report.get("last_generated", datetime(year=1970,month=1, day=1)) < (datetime.now() -timedelta(days=1))):
-        report = generate_engineer_report(name, timeperiod)
-        report['last_generated'] = datetime.now()
-        jirareportsdb.reports.update({"type":"engineer", "name":name, "period":timeperiod}, report, upsert=True, safe=True )
-        del report['last_generated']
-        return json.dumps(report)
-    else:
-        del report['_id']
-        del report['last_generated']
-        return json.dumps(report)
+        report = jirareportsdb.reports.find_one({"type":"engineer", "name":name, "period":timeperiod})
+        if (report is None or
+            report.get("last_generated", datetime(year=1970,month=1, day=1)) < (datetime.now() -timedelta(days=1))):
+            report = generate_engineer_report(name, timeperiod)
+            report['last_generated'] = datetime.now()
+            jirareportsdb.reports.update({"type":"engineer", "name":name, "period":timeperiod}, report, upsert=True, safe=True )
+            del report['last_generated']
+            return json.dumps(report)
+        else:
+            del report['_id']
+            del report['last_generated']
+            return json.dumps(report)
 
 class JiraCustomerReport(CorpBase):
-  def GET(self, name):
-    web.header('Content-type','application/json')
-    pageparams = web.input()
-    timeperiod =  pageparams.get("period", None)
-    report = jirareportsdb.reports.find_one({"type":"customer",'period':timeperiod, 'customer':name})
-    if report is not None:
-        del report['_id']
-        return json.dumps(report)
-    else:
-        report = generate_customer_report(name, timeperiod)
-        jirareportsdb.reports.update({"type":"customer", "name":name, "period":timeperiod}, report, upsert=True, safe=True )
-        return json.dumps(report)
+    def GET(self, name):
+        web.header('Content-type','application/json')
+        pageparams = web.input()
+        timeperiod =  pageparams.get("period", None)
+        report = jirareportsdb.reports.find_one({"type":"customer",'period':timeperiod, 'customer':name})
+        if report is not None:
+            del report['_id']
+            return json.dumps(report)
+        else:
+            report = generate_customer_report(name, timeperiod)
+            jirareportsdb.reports.update({"type":"customer", "name":name, "period":timeperiod}, report, upsert=True, safe=True )
+            return json.dumps(report)
 
 if __name__ == '__main__':
     generate_all_companies()
--- support/jira2mongo.py
+++ support/jira2mongo.py
@@ -9,6 +9,7 @@ import datetime
 import traceback
 import math
 import types
+import time
 
 path = os.path.dirname(os.path.abspath(__file__))
 path = path.rpartition( "/" )[0]
@@ -80,6 +81,7 @@ class Retreiver:
                 issue["comments"] = self.jira.getComments( issue["key"] )
                 issue["resolutiondate"] = self.jira.getResolutionDateByKey(issue["key"])
                 self.issues[issue["key"]] = self.fixCustomFields(issue)
+
         except suds.WebFault as detail:
             print detail
             #If a issue is missing, iterate through 1 at a time.
@@ -101,6 +103,15 @@ class Retreiver:
         del issue.customFieldValues
         return issue
 
+    def retreiveRecent(self, jql):
+        self.updateCustomFields()
+        self.baseJQL = jql
+        yesterday = (datetime.datetime.now() - datetime.timedelta(days=2)).timetuple()
+        query =  "%s AND updated >= %s" % ( jql, time.strftime("%Y-%m-%d", yesterday) )
+        self.retreiveSet(query)
+        time.sleep(1)
+        self.counter = self.counter + self.batchSize
+
     def retreive( self, jql ):
         self.updateCustomFields()
         self.baseJQL = jql
@@ -108,6 +119,7 @@ class Retreiver:
             query =  "%s AND key >= %s-%i AND key < %s-%i" % ( jql, self.projectName, self.counter, self.projectName, self.counter + self.batchSize)
             print query
             self.retreiveSet(query)
+            time.sleep(1)
             self.counter = self.counter + self.batchSize
 
     def display( self):
@@ -119,6 +131,7 @@ class Retreiver:
     def storeIssue( self, issue ):
         i = pythonify_suds_obj(issue)
         i['_id'] = i['key']
+        print "saving issue:", i['key']
         db.issues.save(i)
 
     def store(self):
@@ -134,8 +147,18 @@ class Retreiver:
 def run( retreiver, jql ):
     retreiver.retreive( jql )
 
-retreiver = Retreiver()
-run( retreiver, "project = CS AND type != Tracking" )
-retreiver.store()
-retreiver.updateCustomFields()
 
+def main(args):
+  if "--recent" in args:
+    retreiver = Retreiver()
+    retreiver.retreiveRecent("project = CS AND type != Tracking")
+    retreiver.store()
+    retreiver.updateCustomFields()
+  else:
+    retreiver = Retreiver()
+    run( retreiver, "project = CS AND type != Tracking" )
+    retreiver.store()
+    retreiver.updateCustomFields()
+
+
+if __name__ == '__main__': main(sys.argv[1:])
