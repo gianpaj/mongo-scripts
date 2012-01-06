41dc1f01a185d54fc9fc260f9999c1e58e8acbac
Mike O'Brien <mikeo@10gen.com>
2012-01-05 19:05:44 -0500

    adding last comment to daily CS email

--- support/jiraalert.py
+++ support/jiraalert.py
@@ -12,7 +12,7 @@ import datetime
 import traceback
 
 path = os.path.dirname(os.path.abspath(__file__))
-path = path.rpartition( "/" )[0] 
+path = path.rpartition( "/" )[0]
 sys.path.append( path )
 
 import lib.gmail
@@ -33,15 +33,15 @@ import settings
 # { "jql" : "..." ,           the actual query to run
 #   "who" : "AS" ,            list of types
 #   "sms" : <Bool>            whether to send an sms for this
-#   "digest" : <Bool>         whether or not this should be part of a daily digest. 
+#   "digest" : <Bool>         whether or not this should be part of a daily digest.
 #                             if so, goes out at night rather than immediate
 #   "freq"   :                # of hours betweeen emails  - None means no cap
 #   "filter" :                if True, skip this issue
 # }
 
 csBigFilter = """
-  project = CS AND 
-  resolution = Unresolved AND 
+  project = CS AND
+  resolution = Unresolved AND
   status not in ("Waiting for Customer", "Waiting for bug fix" , "Resolved" ) AND
   type != Tracking
 """
@@ -65,7 +65,7 @@ def last_comment_from_10gen( jira , issue ):
     if comments is None or len(comments) == 0:
         mydebug( "no comments" )
         return False
-    
+
     c = comments[len(comments)-1]
     who = jira.getUser( c["author"] )["email"].lower()
     mydebug( "last comment from %s " % who )
@@ -85,49 +85,49 @@ queries = [
     { "name" : "SLA in danger - blocker needs response" ,
       "who" : "AO" ,
       "digest" : False ,
-      "freq" : 3 , 
+      "freq" : 3 ,
       "filter" : last_comment_from_10gen ,
-      "jql" : csBigFilter + " AND priority = blocker AND updated <= -60m" } , 
-    
+      "jql" : csBigFilter + " AND priority = blocker AND updated <= -60m" } ,
+
     { "name" : "SLA in danger - critical needs response" ,
       "who" : "AO" ,
       "digest" : False ,
       "freq" : 6 ,
       "filter" : last_comment_from_10gen ,
-      "jql" : csBigFilter + " AND priority = critical AND updated <= -180m" } , 
+      "jql" : csBigFilter + " AND priority = critical AND updated <= -180m" } ,
 
     { "name" : "SLA in danger - CS" ,
       "who" : "AO" ,
       "digest" : False ,
       "freq" : 6 ,
       "filter" : last_comment_from_10gen ,
-      "jql" : csBigFilter + " AND  updated <= -12h" } , 
+      "jql" : csBigFilter + " AND  updated <= -12h" } ,
 
 
     # ------ cs bad response issues ----------
 
-    { "name": "CS problems not touched in 24 hours" , 
-      "jql" : csBigFilter + " AND  updated <= -24h AND issuetype = 'Problem Ticket'" , 
-      "who" : "AO" , 
-      "sms" : False , 
+    { "name": "CS problems not touched in 24 hours" ,
+      "jql" : csBigFilter + " AND  updated <= -24h AND issuetype = 'Problem Ticket'" ,
+      "who" : "AO" ,
+      "sms" : False ,
       "digest" : True  } ,
 
-    { "name": "CS questions not touched in 2 days" , 
-      "jql" : csBigFilter + " AND updated <= -48h AND issuetype != 'Problem Ticket'" , 
-      "who" : "AO" , 
-      "sms" : False , 
+    { "name": "CS questions not touched in 2 days" ,
+      "jql" : csBigFilter + " AND updated <= -48h AND issuetype != 'Problem Ticket'" ,
+      "who" : "AO" ,
+      "sms" : False ,
       "digest" : True } ,
 
     # ------ community private ----------
 
-    { "name" : "Community Private not touched in 45 days" , 
+    { "name" : "Community Private not touched in 45 days" ,
       "jql" : "project = SUPPORT AND status in (Open, \"In Progress\", Reopened) and updated <= -45d" ,
       "who" : "AO" ,
       "sms" : False ,
       "digest" : True } ,
 
 
-    { "name" : "Community Private not touched in 3 days" , 
+    { "name" : "Community Private not touched in 3 days" ,
       "jql" : "project = SUPPORT AND status in (Open, \"In Progress\", Reopened) and updated <= -3d" ,
       "who" : "AO" ,
       "sms" : False ,
@@ -136,18 +136,18 @@ queries = [
 
     # ------ debugging with submitted ----------
 
-    { "name" : "SERVER - debugging with suebmitter not touched in a month" , 
+    { "name" : "SERVER - debugging with suebmitter not touched in a month" ,
       "jql" : "project = SERVER AND fixVersion = 'debugging with submitter' AND ( status = Open or status = Reopened ) and updated <= -30d" ,
-      "who" : "A" , 
-      "sms" : False , 
+      "who" : "A" ,
+      "sms" : False ,
       "digest" : True } ,
 
-    { "name" : "SERVER - debugging with submitter not touched in a week" , 
+    { "name" : "SERVER - debugging with submitter not touched in a week" ,
       "jql" : "project = SERVER AND fixVersion = 'debugging with submitter' AND ( status = Open or status = Reopened ) and updated <= -7d" ,
-      "who" : "A" , 
-      "sms" : False , 
+      "who" : "A" ,
+      "sms" : False ,
       "filter" : last_comment_from_10gen ,
-      "digest" : True } 
+      "digest" : True }
 
     ]
 
@@ -163,10 +163,10 @@ inDebug = False
 # ---------------
 # ---------------
 
-supportTriageList = [] 
+supportTriageList = []
 def getSupportTriageList():
     global supportTriageList
-    
+
     if len(supportTriageList) == 0:
         supportTriageList += crowd.findGroupByName( "commercial support triage" )
     return supportTriageList
@@ -185,7 +185,7 @@ def expandWho( issue , who ):
                 continue
             t = "S"
 
-            
+
         if t == "S":
             all += getSupportTriageList()
             continue
@@ -229,7 +229,7 @@ def mail( subject , body , who ):
 def run( digest ):
 
     jira = lib.jira.JiraConnection()
-    
+
     conn = pymongo.Connection('jira.10gen.cc');
     db = conn.jira_alert
     last_alert = db.last_alert
@@ -237,22 +237,22 @@ def run( digest ):
     seenAlready = set()
 
     messages = {}
-    
+
     for q in queries:
 
         def inBlackout( issue ):
             if "freq" not in q:
                 return False
-            
+
             last = db.last_email.find_one( { "_id" : issue["key"] } )
-            
+
             if last is None:
                 if not inDebug:
                     db.last_email.insert( { "_id" : issue["key"] , "last" : datetime.datetime.now() } )
                 return False
 
             diff = datetime.datetime.now() - last["last"]
-            diff = (diff.seconds / 3600.0 + diff.days * 24.0 ) 
+            diff = (diff.seconds / 3600.0 + diff.days * 24.0 )
 
             if q["freq"] > diff:
                 debug( "in blackout key: %s last: %s " % ( issue["key"] , last ) )
@@ -261,42 +261,53 @@ def run( digest ):
             if not inDebug:
                 db.last_email.save( { "_id" : issue["key"] , "last" : datetime.datetime.now() } )
             return False
-    
 
-        
+
+
         name = q["name"]
-        
+
         if digest != q["digest"]:
             continue
 
         for issue in jira.getIssuesFromJqlSearch( q["jql"] , 1000 ):
-            
+
             if issue["key"] in seenAlready:
                 debug( "\t\t\t skipping because already seen" )
                 continue
-            
+
             if not digest and inBlackout( issue ):
                 continue
 
             if "filter" in q and q["filter"]( jira , issue ):
                 debug( "\t\t\t skipping because of filter" )
                 continue
-            
+
             seenAlready.add( issue["key"] )
 
             debug( "%s\t%s" % ( issue["key"] , issue["summary"] ) )
             who = q["who"]
             who = set(expandWho( issue , who ))
             debug( "\t" + str(who) )
-            
+
+            comments = jira.getComments( issue["key"] )
+            if comments is None or len(comments) == 0:
+                issue['latest_comment'] = None
+                issue['latest_commenter'] = None
+            else:
+                # TODO cache the results of JIRA query for comments, since the filter might have called it already
+                latest_comment = comments[len(comments)-1]
+                latest_commenter = jira.getUser( latest_comment["author"] )["email"].lower()
+                issue['latest_comment'] = latest_comment
+                issue['latest_commenter'] = latest_commenter
+
             for w in who:
                 if w not in messages:
                     messages[w] = {}
-                    
+
                 p = messages[w]
                 if name not in p:
                     p[name] = []
-                    
+
                 p[name].append( issue )
 
                 if "sms" in q and q["sms"]:
@@ -317,18 +328,18 @@ def getCompany(issue):
     for x in issue["customFieldValues"]:
         if x["customfieldId"] == "customfield_10030":
             return x["values"][0]
-        
+
     return None
-        
+
 
 def sendEmails( messages , managerSummary , digest ):
-    
+
     mgr = ""
 
-    shortDate = datetime.datetime.now().strftime( "%Y-%b-%d" ) 
-    
+    shortDate = datetime.datetime.now().strftime( "%Y-%b-%d" )
+
     for who in messages:
-        
+
         mgr += who + "\n"
         ind = ""
 
@@ -336,15 +347,19 @@ def sendEmails( messages , managerSummary , digest ):
         for name in mymessages:
             mgr += "\t" + name + "\n"
             ind += name + "\n"
-            
+
             for issue in mymessages[name]:
                 simple = "http://jira.mongodb.org/browse/%s\t%s\t%s\n" % ( issue["key"] , getCompany(issue) , issue["summary"] )
                 mgr += "\t\t" + simple
                 ind += "\t" + simple
-                
+                if issue["latest_comment"] and issue["latest_commenter"]:
+                    mgr += "\n\tLatest Comment: [%s] %s" % (issue["latest_commenter"], issue["latest_comment"]['body'] + "\n\n")
+                    ind += "\n\tLatest Comment: [%s] %s" % (issue["latest_commenter"], issue["latest_comment"]['body'] + "\n\n")
+
+
             mgr += "\n"
             ind += "\n"
-    
+
         debug( "will send email to: %s" % who )
         debug( ind )
         if digest:
@@ -363,7 +378,8 @@ def sendEmails( messages , managerSummary , digest ):
         for s in getSupportTriageList():
             mgre = getEmail(s)
             debug( "sending to manager: %s " % mgre )
-            mail( subject , mgr , mgre )
+            print "to",mgre
+            mail( subject , mgr , "mikeo@10gen.com" )
 
 def test_sms():
     t = lib.sms.Twilio()
@@ -372,7 +388,7 @@ def test_sms():
 if __name__ == "__main__":
 
     digest = False
-    
+
     for x in sys.argv:
         if x == "debug":
             inDebug = True
