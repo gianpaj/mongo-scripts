5d2ad521a1a8e258b515f34578bd568e375b10a5
Gianfranco Palumbo <gianpa@gmail.com>
2013-02-19 15:13:42 +0000

    Added script to tail logfile from the last restart

---
+++ rlogtail.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+logfile=$1
+
+if [ $logfile ] && [[ -e $logfile ]] ; then
+  # if logfile exists:
+  XXX=$(grep -n 'RESTARTED' tailtest.log |tail -1|awk -F: '{print $1}')
+
+  tail -n +$XXX $logfile > $logfile-from_last_restart.log
+else
+  # if logfile does not exist:
+  echo 'The logfile does not exist or is not defined.'
+  echo 'USAGE: rlogtail.sh mongoX.log'
+fi
