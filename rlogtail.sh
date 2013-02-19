6ae3db9b324bafbf14e618f2c72d842dd9964624
Gianfranco Palumbo <gianpa@gmail.com>
2013-02-19 18:28:01 +0000

    Improve performace tailing form the bottom of the logfile with tail -r

--- rlogtail.sh
+++ rlogtail.sh
@@ -4,9 +4,11 @@ logfile=$1
 
 if [ $logfile ] && [[ -e $logfile ]] ; then
   # if logfile exists:
-  XXX=$(grep -n 'RESTARTED' $logfile | tail -1 | awk -F: '{print $1}')
+  XXX=$(tail -r $logfile | grep -n 'RESTARTED' -m 1 | awk -F: '{print $1}')
   if [ $XXX ]; then
-    tail -n +$XXX $logfile > $logfile-from_last_restart.log
+    newlogfile="$(echo $logfile | sed 's/\.log//')-from_last_restart.log"
+    logfilelength=$(wc -l $logfile | awk '{print($1)}')
+    tail -n +$(expr $logfilelength - $XXX) $logfile > $newlogfile
   else
     echo "$logfile does not contain 'RESTARTED'"
   fi
