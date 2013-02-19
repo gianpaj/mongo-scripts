9f1273612078e449b09ae23e1bd6df8a7f723645
Gianfranco Palumbo <gianpa@gmail.com>
2013-02-19 16:00:52 +0000

    add check if logfile contains RESTARTED

--- rlogtail.sh
+++ rlogtail.sh
@@ -5,8 +5,11 @@ logfile=$1
 if [ $logfile ] && [[ -e $logfile ]] ; then
   # if logfile exists:
   XXX=$(grep -n 'RESTARTED' $logfile | tail -1 | awk -F: '{print $1}')
-
-  tail -n +$XXX $logfile > $logfile-from_last_restart.log
+  if [ $XXX ]; then
+    tail -n +$XXX $logfile > $logfile-from_last_restart.log
+  else
+    echo "$logfile does not contain 'RESTARTED'"
+  fi
 else
   # if logfile does not exist:
   echo 'The logfile does not exist or is not defined.'
